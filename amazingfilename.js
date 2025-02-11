b3581748440dc9e89c39688362e9c4b1846965d1
Madison <165460120+grek2@users.noreply.github.com>
2025-02-11 18:29:56 -0500

    Add files via upload

---
+++ amazingfilename.js
@@ -0,0 +1,15 @@
+let TTT = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+]
+
+function displayBoard() {
+    console.log(TTT[0]);
+    console.log(TTT[1]);
+    console.log(TTT[2]);
+}
+
+displayBoard();
+
+
